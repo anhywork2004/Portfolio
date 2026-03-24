e705106cdfd38c416674079c4a3c675d16876287
anhywork2004 <anhy.work.2004@gmail.com>
2026-03-25 00:54:31 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -13,3 +13,30 @@ progresses.forEach((progress) => {
     const width = progress.getAttribute('data-width');
     progress.style.setProperty('--width', width);
 });
+const stats = document.querySelectorAll('.stat-number');
+
+const animateStats = () => {
+    stats.forEach(stat => {
+        let text = stat.innerText;
+        
+        if(text.includes('3')) {
+            let count = 0;
+            let interval = setInterval(() => {
+                count++;
+                stat.innerText = count + "+";
+                if(count >= 3) clearInterval(interval);
+            }, 200);
+        }
+
+        if(text.includes('70M')) {
+            let count = 0;
+            let interval = setInterval(() => {
+                count += 10;
+                stat.innerText = count + "M+";
+                if(count >= 70) clearInterval(interval);
+            }, 50);
+        }
+    });
+};
+
+window.addEventListener('load', animateStats);
